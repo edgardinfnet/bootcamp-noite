74e6f3aa6ff235106797d5cb20e6fdc51e0f4644
Edgard Araujo <edgard.araujo@al.infnet.edu.br>
2023-04-08 21:44:04 -0300

    capturando valores do formulário para validação

--- script/script.js
+++ script/script.js
@@ -1,1 +1,31 @@
-console.log("script funcionando");
+const nomeInput = document.getElementById("nome");
+const telefoneInput = document.getElementById("telefone");
+const emailInput = document.getElementById("email");
+const contatoRadio = document.getElementsByName("contato");
+const mensagemTextArea = document.getElementById("mensagem");
+
+nomeInput.onkeyup = () => {
+    console.log("nomeInput: ", nomeInput.value);
+};
+
+telefoneInput.onkeyup = () => {
+    console.log("telefoneInput: ", telefoneInput.value);
+};
+
+emailInput.onkeyup = () => {
+    console.log("emailInput: ", emailInput.value);
+};
+
+contatoRadio.forEach(
+    radioSelected => {
+        radioSelected.onclick = () => {
+            console.log("contatoRadio: ", radioSelected.value);
+        };
+    }
+);
+
+mensagemTextArea.onkeyup = () => {
+    console.log("mensagemTextArea: ", `${mensagemTextArea.value}`);
+};
+
+
